4089fa1760c9d212b3edb8eb15510236fb516ef7
Nguyễn Quang Trường <quangtruong.gt1@gmail.com>
2023-09-12 21:19:21 +0700

    refactor: set-tvg-id

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ## Set tvg-id
 
 ```bash
-node scripts/m3u.js --path="https://raw.githubusercontent.com/Televison/TV24/main/Project-v3.m3u" --mapping=data/name_id_mapping.json
+node scripts/main.js --path="https://raw.githubusercontent.com/Televison/TV24/main/Project-v3.m3u" --mapping=data/name_id_mapping.json
 ```
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
 import {M3uParser} from 'm3u-parser-generator';
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import { Command } from 'commander';
 import request from 'sync-request';
 
@@ -9,13 +9,16 @@ const program = new Command()
 program
   .requiredOption('--path <config>', 'Path to m3u')
   .requiredOption('--mapping <mapping>', 'Name to id mapping json file')
+  .option('--mode <mode>', 'Running mode', 'set-tvg-id')
+  .option('-o, --output <output>', 'Output path', 'tmp/playlist.m3u')
+  // Mode set-tvg-id
   .option('--replace <replace>', 'Replace existing id', false)
   .parse(process.argv)
 
 const options = program.opts()
 console.log("Opts: ", options)
 
-function loadM3u(path) {
+function loadFileOrUrl(path) {
     if (!path) throw new Error('Path is missing')
     if (path.indexOf("http") == 0) {
         return request('GET', path).getBody('utf8');
@@ -24,22 +27,11 @@ function loadM3u(path) {
     }
 }
 
-const m3u = loadM3u(options.path)
+const m3u = loadFileOrUrl(options.path)
 const playlist = M3uParser.parse(m3u, true);
-const mapping = JSON.parse(readFileSync(options.mapping, 'utf8'));
-
-playlist.medias.forEach(media => {
-    let id = getIdForName(media.name)
-    if (id) {
-        if (options.replace) {
-            media.attributes["tvg-id"] = id
-        } else if (!media.attributes["tvg-id"]) {
-            media.attributes["tvg-id"] = id
-        }
-    }
-});
 
 function getIdForName(channelName) {
+    const mapping = JSON.parse(readFileSync(options.mapping, 'utf8'));
     if (!channelName) {
         return ""
     }
@@ -51,6 +43,26 @@ function getIdForName(channelName) {
     return ""
 }
 
-const m3uString = playlist.getM3uString();
+function setTvgId() {
+    playlist.medias.forEach(media => {
+        let id = getIdForName(media.name)
+        if (id) {
+            if (options.replace) {
+                media.attributes["tvg-id"] = id
+            } else if (!media.attributes["tvg-id"]) {
+                media.attributes["tvg-id"] = id
+            }
+        }
+    });
+    const m3uString = playlist.getM3uString();
+    writeFileSync(options.output, m3uString)
+    console.log("Set tvg-id done. Output file: ", options.output)
+}
 
-console.log(m3uString)
+function main() {
+    if (options.mode === 'set-tvg-id') {
+        setTvgId()
+    }
+}
+
+main()
